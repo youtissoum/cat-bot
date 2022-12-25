32127f67b248359128c76d03f3fc1431d59339f4
youtissoum <youtissoum@outlook.fr>
2022-12-25 23:18:59 +0100

    added all commands that did not change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import nextcord as discord
 from nextcord.ext import commands, tasks
-import os, sys, time, json
+import os, sys, time, json, base64, requests
 
 time_before_start = time.time()
 
@@ -74,6 +74,87 @@ async def on_ready():
             register_member(guild.id, user.id)
     automatic_database_save.start()
     print(f"bot has been started in {time.time()-time_before_start:.2f}s")
+
+@bot.slash_command(description="Send Help")
+async def help(message: discord.Interaction):
+	embedVar = discord.Embed(
+		title="Send Help", description=discord.utils.get(bot.get_guild(GUILD_ID).emojis, name="staring_cat"), color=0x6E593C
+	).add_field(
+		name="Cat Hunt Commands",
+		inline=False,
+		value="**/inv** - your cats\n**/leaderboards** - da cat leaderboad\n**/donate** - donate your cats to another person\n**/achs** - view your achievements\n**/feedback** - give suggestions, report bugs, and everything in between",
+	).add_field(
+		name="Info Commands",
+		inline=False,
+		value="**/random** - get random cat image\n**right click > apps > catch** - catch someone in 4k\n**/tiktok** - read message as tiktok woman tts\n**/help** - this command\n**/admin** - help for server admins\n**/cat** - get staring cat image\n**/info** - get info bout bot and credits",
+	)
+	await message.response.send_message(embed=embedVar)
+
+@bot.slash_command(description="Give feedback, report bugs or suggest ideas")
+async def feedback(message: discord.Interaction, feedback: str):
+	if len(str(message.user) + "\n" + feedback) >= 2000:
+		await message.response.send_message("ah hell nah man, ur msg is too long :skull:", ephemeral=True)
+		return
+	milenakoos = await bot.fetch_user(OWNER_ID)
+	await milenakoos.send(str(message.user) + "\n" + feedback)
+	await message.response.send_message("your feedback was directed to the bot owner!", ephemeral=True)
+
+@bot.slash_command(description="View admin help", default_member_permissions=8)
+async def admin(message: discord.Interaction):
+	embedVar = discord.Embed(
+		title="Send Admin Help", description=discord.utils.get(bot.get_guild(GUILD_ID).emojis, name="staring_cat"), color=0x6E593C
+	).add_field(name="Admin Commands", value="**/setup** - makes cat bot send cats in the channel this command is ran in\n**/forget** - reverse of /setup (i forgor :skull:)\n**/summon** - makes cats disappear and reappear out of thin air\n**/giveach** - gib (or take) achievements to people\n**/force** - makes cat appear in chat\n**/say** - chat as cat\n**/reset** - fully resets one's account")
+	await message.response.send_message(embed=embedVar)
+
+@bot.slash_command(description="View information about the bot")
+async def info(message: discord.Interaction):
+	embedVar = discord.Embed(title="Cat Bot", color=0x6E593C, description="[Join support server](https://discord.gg/WCTzD3YQEk)\n[GitHub Page](https://github.com/milena-kos/catbot)\n\nBot made by Milenakos#3310\nThis bot adds Cat Hunt to your server with many different types of cats for people to discover! People can see leaderboards and give cats to each other.\n\nThanks to:\n**???** for the cat image\n**SLOTHS2005#1326** for getting troh to add cat as an emoji\n**aws.random.cat** for random cats API\n**@weilbyte on GitHub** for TikTok TTS API\n**TheTrashCell#0001** for making cat, suggestions, and a lot more.\n\n**CrazyDiamond469#3422, Phace#9474, SLOTHS2005#1326, frinkifail#1809, Aflyde#3846, TheTrashCell#0001 and Sior Simotideis#4198** for being test monkeys\n\n**And everyone for the support!**")
+	await message.response.send_message(embed=embedVar)
+
+@bot.slash_command(description="Read text as TikTok's TTS woman")
+async def tiktok(message: discord.Interaction, text: str):
+	stuff = requests.post("https://tiktok-tts.weilnet.workers.dev/api/generation", headers={"Content-Type": "application/json"}, json={"text": text, "voice": "en_us_002"})
+	try:
+		data = "" + stuff.json()["data"]
+	except TypeError:
+		await message.response.send_message("i dont speak your language (remove non-english characters)", ephemeral=True)
+		return
+	with open("result.mp3", "wb") as f:
+		ba = "data:audio/mpeg;base64," + data
+		f.write(base64.b64decode(ba))
+	file = discord.File("result.mp3", filename="result.mp3")
+	await message.response.send_message(file=file)
+
+@bot.slash_command(description="Pong")
+async def ping(message: discord.Interaction):
+	await message.response.send_message(f"cat has brain delay of {round(bot.latency * 1000)} ms " + str(discord.utils.get(bot.get_guild(GUILD_ID).emojis, name="staring_cat")))
+
+@bot.slash_command(description="Get Cat")
+async def cat(message: discord.Interaction):
+	file = discord.File("cat.png", filename="cat.png")
+	await message.response.send_message(file=file)
+
+@bot.slash_command(description="Get Cursed Cat")
+async def cursed(message: discord.Interaction):
+	file = discord.File("cursed.jpg", filename="cursed.jpg")
+	await message.response.send_message(file=file)
+
+@bot.slash_command(description="Get Your balance")
+async def bal(message: discord.Interaction):
+	file = discord.File("money.png", filename="money.png")
+	embed = discord.Embed(title="cat coins", color=0x6E593C).set_image(url="attachment://money.png")
+	await message.response.send_message(file=file, embed=embed)
+
+@bot.message_command()
+async def pointLaugh(message: discord.Interaction, msg):
+	icon = discord.utils.get(bot.get_guild(GUILD_ID).emojis, name="pointlaugh")
+	await msg.add_reaction(icon)
+	await message.response.send_message(icon, ephemeral=True)
+
+@bot.slash_command(description="Say stuff as cat", default_member_permissions=8)
+async def say(message: discord.Interaction, text: str):
+	await message.response.send_message("success", ephemeral=True)
+	await message.channel.send(text)
 
 try:
     bot.run(TOKEN)
