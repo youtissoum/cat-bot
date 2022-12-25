fe55640779b48a8209980689d9960a9dcf9c3c8e
youtissoum <youtissoum@outlook.fr>
2022-12-25 22:54:46 +0100

    sync git

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import nextcord as discord
-from nextcord.ext import commands
-import os, sys, time
+from nextcord.ext import commands, tasks
+import os, sys, time, json
 
 time_before_start = time.time()
 
@@ -14,9 +14,28 @@ if TOKEN == None:
     print("Could not find token, exiting...")
     sys.exit(1)
 
+with open("db.json", "r") as f:
+    db = json.loads(f.read())
+
+with open("aches.json", 'r') as f:
+    ach_list = json.loads(f.read())
+
+with open("cats.json", "r") as f:
+    cats = json.loads(f.read())
+
 intents = discord.Intents.default()
 intents.message_content = True
-bot = commands.Bot(command_prefix="", help_command=None, intents=intents)
+bot = commands.Bot(command_prefix="idk how this works but you need to have spaces in it or it may crash", help_command=None, intents=intents)
+
+def save_db():
+    with open("backup", "r") as f:
+        f.write(str(db))
+    with open("db.json", 'w') as f:
+        f.write(json.dumps(db))
+
+@tasks.loop(seconds = 10)
+async def automatic_database_save():
+    save_db()
 
 @bot.event
 async def on_ready():
@@ -26,6 +45,12 @@ async def on_ready():
             name=f"/help | Providing life support for {len(bot.guilds)} servers"
         )
     )
+    automatic_database_save.start()
     print(f"bot has been started in {time.time()-time_before_start:.2f}s")
 
-bot.run(TOKEN)
+try:
+    bot.run(TOKEN)
+except Exception as e:
+    save_db() # save if exception occured
+    raise e
+save_db() # save if exited succesfully
